45cd5ee812133be344eafcab1da315acfc6ed2dd
remi-wambeke <remi.wambeke@edu.esiee.fr>
2025-12-11 08:22:18 +0000

    code final de 07-ASCIIART

--- main.py
+++ main.py
@@ -19,8 +19,18 @@ def artcode_i(s):
     """
     
     # votre code ici
-
-    return [ ]
+    resultat = []
+    compteur = 1
+    caractere_en_cours = s[0]
+    for k in range(1, len(s)): 
+        if s[k] == caractere_en_cours :
+            compteur += 1
+        else :
+            resultat.append((caractere_en_cours, compteur))
+            caractere_en_cours = s[k]
+            compteur = 1
+    resultat.append((caractere_en_cours, compteur))
+    return resultat
 
 
 def artcode_r(s):
@@ -38,8 +48,13 @@ def artcode_r(s):
     # cas de base
     # recherche nombre de caractères identiques au premier
     # appel récursif
-
-    return []
+    if len(s)==0:
+        return[]
+    compteur = 1
+    while compteur < len(s) and s[compteur]==s[0]:
+        compteur += 1
+    premier_tuple = (s[0], compteur)
+    return [premier_tuple] + artcode_r(s[compteur:])
     
 
 #### Fonction principale
